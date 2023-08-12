83b52596610a8b894233fb09654b25029a471dde
KBandipo <keneebandipo@gmail.com>
2023-08-11 17:21:19 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -main function
+ * Return: 0
+ */
+int main(void)
+{
+	char alph;
+		for (alph = 'z'; alph >= 'a'; alph--)
+	{putchar(alph); }
+	putchar('\n');
+	return (0);
+}

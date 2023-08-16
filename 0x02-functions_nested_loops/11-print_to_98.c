db3ae02f4b300d5b975657bee2eb22ce0d6bd6b1
KBandipo <keneebandipo@gmail.com>
2023-08-16 02:57:17 -0700

    11-print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,36 +1,45 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *
- *
+ * print_to_98 - Print from n to 98
+ * @n: Parameter
+ * Return: void
  */
 void print_to_98(int n)
 {
 	int i;
-	if (n >= 0 && n <= 98)
-	{
+		if (n >= 0 && n <= 98)
+		{
+			for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+		printf("\n");
+		}
+		else if (n < 0)
+		{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
 		}
 		printf("\n");
-	}
-	else if (n < 0)
-	{
-		for (i < n; i < 98; i++)
+		}
+		else if (n > 98)
 		{
-			printf("%d  ", i);
-		}
-		printf("\n");
-	}
-	else if (n > 98)
-	{
 		for (i = n; i >= 98; i--)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
 		}
-	printf("\n");
-	}
-	else if (n ==98)
-		printf("%d\n", n);
+			printf("\n");
+		}
+		else if (n == 98)
+		{
+			printf("%d\n", n);
+		}
 }

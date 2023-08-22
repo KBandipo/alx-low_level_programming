49e1557a9c0e3bb66c84ecb05827eaeecbe381ae
KBandipo <keneebandipo@gmail.com>
2023-08-21 18:04:29 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *   main - Generates random valid passwords for the
+ *   program 101-crackme.
+ *   Return: void
+ */
+int main(void)
+{
+}
